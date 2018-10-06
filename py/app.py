6e6fa3fdf43453ec2e786f32d93ec8d2e87f1b3d
Ana Colautti <ana@easytech.com.ar>
2018-10-06 16:17:56 -0300

    aggrego fibonacci recursivo en python

--- py/app.py
+++ py/app.py
@@ -3,15 +3,20 @@ import time
 
 app = Flask(__name__)
 
+def fibR(n):
+  if n==1 or n==2:
+    return 1
+  return fibR(n-1)+fibR(n-2)
+
+ 
 @app.route("/")
 def root():
     return "rapido como la luz\n"
 
 
 @app.route("/slow")
-def root_time():
-    time.sleep( 1 )
-    return "calculines\n"
+def slow():
+    return "Fibonacci de 30: " + str(fibR(30)) + "\n"
 
 
 @app.route("/delay")
